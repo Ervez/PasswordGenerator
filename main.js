4fe8a309f34f27a27a9d3879a79ff40aa8e65bfc
Ervez <fb.blaszkiewicz@gmail.com>
2023-02-05 19:52:55 +0100

    Initial

---
+++ main.js
@@ -0,0 +1,72 @@
+$(function() {
+    const options = document.querySelectorAll('.settingsOption input');
+    let passwordInputValue = document.getElementById("psswrdInput");
+    const char ={
+        lowercase: "abcdefghijklmnopqrstuvwxyz",
+        uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        numbers: "0123456789",
+        symbols: "!@#$%^&*()_+?>."
+    }
+
+    $('#slider').on("input", function() {
+        $('.sliderValue').text($(this).val());
+        passwordLength = $(this).val();
+        generatePassword();
+    });
+    
+    $( "#buttonGenerate" ).click(function() {
+        $("#copyInfoSpan").removeClass("copyInfoClass");
+        setTimeout( function(){ 
+        $("#copyInfoSpan").addClass("copyInfoClass");
+        $("#copyInfoSpan").removeClass("visible");
+        generatePassword();
+        $("#copyInfoSpan").addClass("visible");
+        $(".flip-card").addClass("rotate");
+        $(".flip-card-inner").addClass("rotate");
+        setTimeout( function(){ 
+            $(".flip-card").removeClass("rotate");
+            $(".flip-card-inner").removeClass("rotate");
+          }  , 2000 );
+        }, 100);
+
+    });
+
+    $(".settingsOption input").click(function(){
+        generatePassword();
+    });
+    
+    $(".containerInput").click(function(){
+        if(passwordInputValue.value != null){
+            $("#copyInfoSpan").addClass("backgroundGreen");
+            copyToClipboard();
+            setTimeout( function(){ 
+            $("#copyInfoSpan").removeClass("visible");
+            $("#copyInfoSpan").removeClass("backgroundGreen");
+            }, 2000);
+        }
+    })
+
+    function copyToClipboard(){
+        passwordInputValue.select();
+        passwordInputValue.setSelectionRange(0, 99999); 
+        navigator.clipboard.writeText(passwordInputValue.value);
+    }
+
+    const generatePassword = () => {
+        let charsForPassword = "";
+        let generatedPassword = "";
+        let passwordLength = $("#slider").val();
+        options.forEach(option => {
+            if(option.checked)
+            {
+                charsForPassword += char[option.id];
+            }
+        });
+        for (let i = 0; i < passwordLength; i++){
+            generatedPassword += charsForPassword[Math.floor(Math.random() * charsForPassword.length)];
+        }
+        $("#psswrdInput").val(generatedPassword);
+    }
+});
+
+
